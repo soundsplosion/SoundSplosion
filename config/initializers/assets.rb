46455ef4f4fb79cc7159af56caf437db7120b161
Timothy A Winchester <twinches@lab1-31.eng.utah.edu>
2015-01-16 17:29:52 -0700

    switching over to ticks, strange bugs still remain

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( shadowbox.js )
 Rails.application.config.assets.precompile += %w( shadowbox.css )
+Rails.application.config.assets.precompile += %w( SortedList.js )
