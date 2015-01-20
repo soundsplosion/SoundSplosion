41795b2480f24bb544b0fcd22e34e0f0e14ef93f
Timothy A Winchester <twinches@lab1-31.eng.utah.edu>
2015-01-19 18:09:56 -0700

    Note selection/deletion working

--- app/assets/javascripts/drawing.js
+++ app/assets/javascripts/drawing.js
@@ -13,3 +13,10 @@ function drawRect(context, coords, fillcolor, linecolor, linewidth){
 function drawNote(context, note){
 	drawRect(context, {left: (note.tickstart / 3), top: (note.keyValue * 23 + 3), right: (note.tickduration / 3), bottom: 23}, note.color, "#000044", 5);
 }
+
+function drawSelectedNote(context, note){
+	drawRect(context, {left: (note.tickstart / 3), top: (note.keyValue * 23 + 3), right: (note.tickduration / 3), bottom: 23}, "#66FF66", "#000044", 5);
+}
+function eraseNote(context, note){
+	context.clearRect((note.tickstart / 3) - 1, (note.keyValue * 23 + 3)-1, (note.tickduration / 3)+2, 25);
+}
