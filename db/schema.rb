f27e2959d84c3e686172dedff5ec8587c9f38907
jeonguny <wn28700@gmail.com>
2015-02-05 10:44:57 -0700

    migrate to postgresql from sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129220451) do
+ActiveRecord::Schema.define(version: 20150205174036) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -26,9 +29,9 @@ ActiveRecord::Schema.define(version: 20150129220451) do
     t.datetime "updated_at"
   end
 
-  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
-  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type"
-  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type"
+  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
+  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
+  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
 
   create_table "comments", force: true do |t|
     t.string   "body"
@@ -42,8 +45,8 @@ ActiveRecord::Schema.define(version: 20150129220451) do
 
   create_table "competitions", force: true do |t|
     t.string   "title"
-    t.datetime "startDate"
-    t.datetime "endDate"
+    t.datetime "startdate"
+    t.datetime "enddate"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "constraints"
@@ -71,8 +74,8 @@ ActiveRecord::Schema.define(version: 20150129220451) do
     t.datetime "updated_at"
   end
 
-  add_index "ratings", ["track_id"], name: "index_ratings_on_track_id"
-  add_index "ratings", ["user_id"], name: "index_ratings_on_user_id"
+  add_index "ratings", ["track_id"], name: "index_ratings_on_track_id", using: :btree
+  add_index "ratings", ["user_id"], name: "index_ratings_on_user_id", using: :btree
 
   create_table "tracks", force: true do |t|
     t.string   "title"
@@ -106,7 +109,7 @@ ActiveRecord::Schema.define(version: 20150129220451) do
     t.datetime "avatar_updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
