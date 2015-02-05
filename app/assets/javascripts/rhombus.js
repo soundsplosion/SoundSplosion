6093cc2f40623868c700b0941fd7953ff1e1224b
timgrant <tim@rootdown.net>
2015-02-04 19:38:18 -0700

    Updating rhombus.js with added Pattern length property.

--- app/assets/javascripts/rhombus.js
+++ app/assets/javascripts/rhombus.js
@@ -598,12 +598,23 @@
       this._name = "Default Pattern Name";
 
       // pattern structure data
+      this._length = 1920;
       this._noteMap = {};
     };
 
     // TODO: make this interface a little more sanitary...
     //       It's a rather direct as-is
     r.Pattern.prototype = {
+
+      getLength: function() {
+        return this._length;
+      },
+
+      setLength: function(length) {
+        if (length !== undefined && length >= 0)
+          this._length = length;
+      },
+
       addNote: function(note) {
         this._noteMap[note._id] = note;
       },
@@ -863,6 +874,7 @@
         var newPattern = new r.Pattern(pattern._id);
 
         newPattern._name = pattern._name;
+        newPattern._length = pattern._length;
 
         // dumbing down Note (e.g., by removing methods from its
         // prototype) might make deserializing much easier
@@ -992,11 +1004,8 @@
         //       based on the current playback position
         for (var playlistId in track._playlist) {
           var ptnId   = track._playlist[playlistId]._ptnId;
-          var noteMap = r._song._patterns[ptnId]._noteMap;
           var offset  = track._playlist[playlistId]._start;
-
-          // TODO: Handle note start and end times relative to the start of
-          //       the originating playlist item
+          var noteMap = r._song._patterns[ptnId]._noteMap;
 
           // TODO: find a more efficient way to determine which notes to play
           if (r.isPlaying()) {
