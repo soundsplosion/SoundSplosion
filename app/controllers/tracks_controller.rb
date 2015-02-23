ef894184e9e6e1a4898a0013f5f000254482a661
jeonguny <wn28700@gmail.com>
2015-02-22 21:37:19 -0700

    load more track when scrolled to the bottom

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -6,7 +6,7 @@ class TracksController < ApplicationController
   # GET /tracks
   # GET /tracks.json
   def index
-    @tracks = Track.all
+    @tracks = Track.all.paginate(:per_page => 4, :page => params[:page])
   end
 
   # GET /tracks/1
