b0eba23ed7c2ce69f9f1b98824936aad72f77134
jeonguny <wn28700@gmail.com>
2015-02-23 20:09:05 -0700

    Bug 157 fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,2 @@
 # Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_SoundSplosion_session'
+SoundSplosion::Application.config.session_store :active_record_store, :key => '_my_app_session'
