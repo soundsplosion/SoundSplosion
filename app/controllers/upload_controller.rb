b6d630a856448b603ed75d90332e170f95173c19
Jeongun Yu <jeonguny@soundsplosion.eng.utah.edu>
2015-03-05 10:01:09 -0700

    block editing track that's not yours

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,14 +1,19 @@
 class UploadController < ApplicationController
-	def new
-	  track_id = params[:track_id]
-    if !Track.find(track_id).competition_id.nil?
+   def new
+    track_id = params[:track_id]
+    track = Track.find(track_id)
+    if !can_edit(track)
       render :status => 400
     end
-  	data = params[:track_data]
+    data = params[:track_data]
     File.open(Rails.root.join('public', 'uploads', track_id), 'wb') do |file|
       file.write(data)
     end
     flash[:notice] = "Your track has been saved"
     redirect_to upload_index_path
+  end
+
+  def can_edit(track)
+    current_user && current_user.id == track.user_id && track.competition_id.nil
   end
 end
