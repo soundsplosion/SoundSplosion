f63c902623de8e54aaac7787c4301cf648b4c517
Jeongun Yu <jeonguny@soundsplosion.eng.utah.edu>
2015-03-06 12:57:01 -0700

    .

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -89,41 +89,41 @@ class TracksController < ApplicationController
     # Check number of tracks constraint
     numTracks = parsed["_tracks"]["_slots"].length 
     if (!@competition.max_tracks.nil? && numTracks > @competition.max_tracks)
-      render text: "INVALID:You exceeded maximum number of tracks parameter!"
+      render text: "INVALID:You exceeded maximum number of tracks parameter!" and return
     end
 
     if (!@competition.min_tracks.nil? && numTracks < @competition.min_tracks)
-      render text: "INVALID:You don't have minimum number of tracks required!"
+      render text: "INVALID:You don't have minimum number of tracks required!" and return
     end
 
     # Check number of instruments constraint
     numInstruments = parsed["_instruments"]["_slots"].length 
     if (!@competition.max_instruments.nil? && numInstruments > @competition.max_instruments)
-      render text: "INVALID:You exceeded maximum number of instruments parameter!"
+      render text: "INVALID:You exceeded maximum number of instruments parameter!" and return
     end
 
     if (!@competition.min_instruments.nil? && numInstruments <  @competition.min_instruments)
-      render text: "INVALID:You haven't used minimum number of instruments!"
+      render text: "INVALID:You haven't used minimum number of instruments!" and return
     end
 
     # Check number of notes constraint
-    numNotes = parsed["_patterns"].count 
+    numNotes = parsed["_patterns"].length
     if (!@competition.min_notes.nil? && numNotes < @competition.min_notes)
-      render text: "INVALID:You haven't used minimum number of notes!"
+      render text: "INVALID:You haven't used minimum number of notes!" and return
     end
     
     if (!@competition.max_notes.nil? && numNotes > @competition.max_notes)
-      render text: "INVALID:You exceeded maximum number of notes!"
+      render text: "INVALID:You exceeded maximum number of notes!" and return
     end
 
     # Check number of effects constraint
-    numEffects = parsed["_effects"].counts 
+    numEffects = parsed["_effects"].length 
     if (!@competition.min_effects.nil? && numEffects < @competition.min_effects)
-      render text: "INVALID:You haven't used minimum number of effects!"
+      render text: "INVALID:You haven't used minimum number of effects!" and return
     end
 
     if (!@competition.max_effects.nil? && numEffects > @competition.max_effects)
-      render text: "INVALID:You exceeded maximum number of effects!"
+      render text: "INVALID:You exceeded maximum number of effects!" and return
     end
     render text: "VALID"
   end
