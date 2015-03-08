999a9c3fff0e04f6f6f9faeec0e0eb2cb03f8b83
Jeongun Yu <jeonguny@soundsplosion.eng.utah.edu>
2015-03-08 14:26:26 -0600

    use will paginate in user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 class UserController < ApplicationController
   include CommonMethods
-  helper_method :get_rating
+  helper_method :get_average_rating
   helper_method :competition_rank
 
   def show
@@ -10,7 +10,7 @@ class UserController < ApplicationController
     @liked_tracks = Track.where(id: @user.likes.pluck(:track_id)).paginate(:per_page => 5, :page => params[:like]).order('id DESC')
     @favorited_tracks = Track.where(id: @user.favorites.pluck(:track_id)).paginate(:per_page => 5, :page => params[:favorite]).order('id DESC')
     @my_tracks = @user.tracks.paginate(:per_page => 5, :page => params[:my]).order('id DESC')
-    @my_entries = @my_tracks.where('competition_id IS NOT NULL').paginate(:per_page => 3, :page => params[:entry])
+    @my_entries = @my_tracks.where('competition_id IS NOT NULL').paginate(:per_page => 5, :page => params[:entry])
 
     respond_to do |format|
         format.html # show.html.erb
