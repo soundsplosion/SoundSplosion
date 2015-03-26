a026cd9b5fdf85b3915d01fa44aec5f20fa6d01c
Timothy Winchester <u0825347@utah.edu>
2015-03-26 17:49:34 -0600

    changed editableclock to use stringToTicks and ticksToString

--- app/assets/javascripts/time.js
+++ app/assets/javascripts/time.js
@@ -1,23 +1,85 @@
-function ticks_to_musical_time(_ticks){ //, displaySettings){
+function ticks_to_musical_time(_ticks, isPos){ //, displaySettings){
   var ticks = _ticks;
+  var offset = (isDefined(isPos) && isPos) ? 1 : 0;
 
   var beat_ticks = Math.floor((480 * 4) / 4); //displaySettings.timesig_den);
   var measure_ticks = beat_ticks * 4; // displaySettings.timesig_num;
   var q_beat_ticks = Math.floor(beat_ticks / 4);
   var beat, q_beat;
 
-  var measure = Math.floor(ticks / measure_ticks) + 1;
+  var measure = Math.floor(ticks / measure_ticks) + offset;
   ticks = ticks % measure_ticks;
 
-  beat = Math.floor(ticks / beat_ticks) + 1;
+  beat = Math.floor(ticks / beat_ticks) + offset;
   ticks = ticks % beat_ticks;
 
-  q_beat = Math.floor(ticks / q_beat_ticks) + 1;
+  q_beat = Math.floor(ticks / q_beat_ticks) + offset;
   ticks = ticks % q_beat_ticks;
 
   ticks = Math.floor(ticks);
 
   return {"measure": measure, "beat": beat, "quarter_beat": q_beat, "ticks": ticks};
+}
+
+function ticksToString(ticks, isPos){
+  var clock = ticks_to_musical_time(ticks, isPos);
+  var measure = (clock.measure + "" === "") ? "0" : clock.measure;
+  var beat = (clock.beat + "" === "") ? "0" : clock.beat;
+  var quarter_beat = (clock.quarter_beat + "" === "") ? "0" : clock.quarter_beat;
+  var ticks = (clock.ticks + "" === "") ? "0" : clock.ticks;
+  return (measure + "." + beat + "." + quarter_beat + "." + ticks);
+}
+
+function stringToTicks(timeString, isPos) {
+  var bar = 0;
+  var beat = 0;
+  var qtrBeat = 0;
+  var ticks = 0;
+
+  var tokens = timeString.split(/(\D+)/);
+  var parsed = new Array(4);
+
+  var offset = (isDefined(isPos) && isPos) ? 1 : 0;
+
+  for (var i = 0; i < tokens.length; i++) {
+    var token = tokens[i];
+
+    // handle even tokens
+    if (((i + 1) % 2) == 1) {
+      if (isInteger(+token) && +token >= 0) {
+        parsed[Math.floor(i/2)] = +token - offset;
+      }
+      else {
+        return undefined;
+      }
+    }
+    // odd tokens must be a single period
+    else {
+      if (token !== '.') {
+        return undefined;
+      }
+    }
+  }
+
+  var ticks = 0;
+
+  if (isDefined(parsed[0])) {
+    ticks += parsed[0] * 1920;
+  }
+
+  if (isDefined(parsed[1])) {
+    ticks += parsed[1] * 480;
+  }
+
+  if (isDefined(parsed[2])) {
+    ticks += parsed[2] * 120;
+  }
+
+  if (isDefined(parsed[3])) {
+    ticks += parsed[3] + offset;
+  }
+
+  return ticks;
 }
 
 function musical_time_to_ticks(musical_time){ //, displaySettings){
