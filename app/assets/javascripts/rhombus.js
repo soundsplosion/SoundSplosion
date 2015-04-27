e94f05cce6589dd046f90fee3d4b1021e8a0f60b
Spencer Phippen <spencer.phippen@gmail.com>
2015-04-27 01:17:34 -0600

    Fixed instrument display names

--- app/assets/javascripts/rhombus.js
+++ app/assets/javascripts/rhombus.js
@@ -1610,6 +1610,8 @@ Rhombus._synthNameMap = {};
 })();
 
 Rhombus._sampleNameList = [
+  ["drums1",         "Drums"],
+  ["drums2",         "808"],
   ["tron_flute",     "Flute"],
   ["tron_woodwinds", "Woodwinds"],
   ["tron_brass_01",  "Brass 01"],
