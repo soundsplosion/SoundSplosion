7b82822cdadc0c957f23a339c61d7cc99b1f2181
Spencer Phippen <spencer.phippen@gmail.com>
2015-04-29 00:33:26 -0600

    Fixed rhombus bug where automation would get applied where it shouldn't have been

--- app/assets/javascripts/rhombus.js
+++ app/assets/javascripts/rhombus.js
@@ -4432,11 +4432,11 @@ Rhombus.prototype.getSong = function() {
 
               var time = ev.getTime() + itemStart;
 
-              if (!loopOverride && r.getLoopEnabled() && start < loopStart) {
+              if (!loopOverride && r.getLoopEnabled() && time < loopStart) {
                 continue;
               }
 
-              if (start >= itemEnd) {
+              if (time >= itemEnd) {
                 continue;
               }
 
