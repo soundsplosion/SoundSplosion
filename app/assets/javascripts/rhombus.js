e8207328f8f926692eb45f122f40ce6a931be5ad
Spencer Phippen <spencer.phippen@gmail.com>
2015-04-29 11:32:18 -0600

    Updated rhombus

--- app/assets/javascripts/rhombus.js
+++ app/assets/javascripts/rhombus.js
@@ -3257,6 +3257,13 @@ Rhombus.Pattern.prototype.getAllNotes = function() {
 };
 
 Rhombus.Pattern.prototype.getNotesInRange = function(start, end, ignoreEnds) {
+  if (isNaN(start) || isNaN(end)) {
+    return [];
+  }
+  if (start > end) {
+    return [];
+  }
+
   // only consider the start tick
   if (isDefined(ignoreEnds) && ignoreEnds === true) {
     return this._noteMap._avl.betweenBounds({ $lt: end, $gte: start });
@@ -3338,6 +3345,13 @@ Rhombus.Pattern.prototype.clearSelectedNotes = function() {
 };
 
 Rhombus.Pattern.prototype.getAutomationEventsInRange = function(start, end) {
+  if (isNaN(start) || isNaN(end)) {
+    return [];
+  }
+  if (start > end) {
+    return [];
+  }
+
   return this._automation.betweenBounds({ $lt: end, $gte: start });
 };
 
